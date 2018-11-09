6bb18d9c015fb5861a52ce5b1859ae754f48f624
Liang Xingchen <liang@maichong.it>
2018-11-09 19:52:30 +0800

    Types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,12 @@
-interface Options {
-  specials?: string | boolean;
-  numbers?: string | boolean;
-  letters?: string | boolean;
+
+declare namespace stringRandom {
+  interface Options {
+    specials?: string | boolean;
+    numbers?: string | boolean;
+    letters?: string | boolean;
+  }
 }
 
-declare function stringRandom(length?: number, options?: Options | string | true): string;
+declare function stringRandom(length?: number, options?: stringRandom.Options | string | true): string;
 
 export = stringRandom;
